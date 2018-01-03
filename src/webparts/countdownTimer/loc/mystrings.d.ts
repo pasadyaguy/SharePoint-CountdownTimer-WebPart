89147aff69d64bbd0341738c76e9d59b22b6cdf2
smithbrianscott <smithbrianscott23@gmail.com>
2018-01-02 19:09:17 -0500

    Initial commit

---
+++ src/webparts/countdownTimer/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICountdownTimerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CountdownTimerWebPartStrings' {
+  const strings: ICountdownTimerWebPartStrings;
+  export = strings;
+}
